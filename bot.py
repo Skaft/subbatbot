0cfcfa5a300162b6c0ed1463c502bca8012b6a1c
Sedsarq <s3dsarq@gmail.com>
2020-08-21 13:58:24 +0200

    First commit (again)!

---
+++ bot.py
@@ -0,0 +1,230 @@
+import os
+from twitchio.ext.commands import Bot, command, errors, check
+from aio_lookup import ChessComAPI, LichessAPI, APIError, UserNotFound
+from sheet import BattleSheet, all_sheet_names
+import aiohttp
+from random import choice
+from string import Template
+
+#Kinda urgent:
+#TODO: Figure out if the _nowait keyword should be used (is it operating in sync now?)
+#TODO: Setting: Disable ?link, pass through whisper
+#       - Bot can whisper to users if apply succeeded
+#TODO: Persistent channel settings
+#       - aiosqlite, or whatever it's called. async storage.
+#TODO: (DONE BUT TEST THIS) Pleb who applies, subscribes, then reapplies should cause a bug of altering someone else's row on sub sheet
+#TODO: On format setting change, modify sheet accordingly
+#TODO: Cloud hosting!
+#TODO: Update help on bot and about page
+#TODO: Git setup
+#TODO: Sheet tests
+
+#Kinda not so urgent:
+#TODO: users_on_sheet *should* go by user id, not display_name
+#       - But: requires DB in order to restore on restart
+#TODO: ?set procedure is icky
+#TODO: logging
+#TODO: Custom prefixes
+#TODO: setting for variants
+#TODO: Auto-follow (for followers-only chat)
+#       - maybe not - would need some scope thing in auth. manual works for now
+#TODO: Use errors.BadArgument, possibly in custom Command classes, to define valid settings?
+#TODO: Custom Command (at least) for apply, to use @error and separate away the error handling.
+#TODO: Dynamic header (so that set_site can alter one column name, set_game another)
+
+
+greetings = [
+    "/me Is it a bird, is it a plane, etc.",
+    "/me is here!",
+    "/me You rang?",
+    "/me I'm Winston Wolf. I solve problems.",
+    "/me Oh sheet!",
+    "/me TO WAR!",
+    "/me Fight to the death!",
+    "/me Hey, I'm on your side. But also maybe on theirs.",
+    "/me *sneaks in*",
+]
+
+DEV_MODE = True
+SED_ID = 88128608
+
+
+def mod_or_sed(ctx):
+    user = ctx.author
+    return user.is_mod or user.id == SED_ID
+
+
+def is_bot_channel(ctx):
+    return ctx.channel.name == 'subbatbot'
+
+
+class SubBatBot(Bot):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_check(mod_or_sed)
+
+        self.session = None
+        self.sheets = {}
+
+        # create a template for help message (prefix may vary)
+        public_commands = ['help', 'clear', 'link', 'apply', 'leave', 'set']
+        docstrings = [cmd._callback.__doc__ for cmd in self.commands.values() if cmd.name in public_commands]
+        command_help = '; '.join("${prefix}" + doc for doc in docstrings)
+        self.help_msg_template = Template(f"Commands: {command_help}")
+
+    async def event_ready(self):
+        self.session = aiohttp.ClientSession()  # needs to be created in async function, hence not in __init__
+        self.apis = {
+            'lichess': LichessAPI(self.session),
+            'chess.com': ChessComAPI(self.session)
+        }
+        if DEV_MODE:
+            channel_names = ['subbatbot', 'sedsarq']
+        else:
+            # TODO: Collect initial channel list from DB instead
+            channel_names = await all_sheet_names()
+        for channel_name in channel_names:
+            await self.join_channel(channel_name)
+        print(f"{os.environ['BOT_NICK']} is online!")
+
+    async def join_channel(self, channel_name):
+        await self.join_channels([channel_name])
+        self.sheets[channel_name] = await BattleSheet.open(channel_name)
+        await self._ws.send_privmsg(channel_name, choice(greetings))
+
+    async def leave_channel(self, channel_name):
+        await self.part_channels([channel_name])
+        sheet = self.sheets.pop(channel_name)
+        await sheet.remove()
+
+    async def event_message(self, msg):
+        if msg.author.name.lower() == os.environ['BOT_NICK'].lower():
+            return
+        try:
+            await self.handle_commands(msg)
+        except errors.MissingRequiredArgument as e:
+            print(e)
+
+    async def event_command_error(self, ctx, error):
+        user = ctx.author
+        name = user.display_name
+        pre = ctx.prefix
+        cmd = f"{pre}{ctx.command.name}"
+        if isinstance(error, errors.CheckFailure):
+            if str(error).endswith('mod_or_sed'):
+                msg = f"Only the {pre}apply command is available to non-moderators, sorry!"
+                return await ctx.send(f"@{name}: {msg}")
+        elif isinstance(error, errors.CommandNotFound):
+            if user.is_mod:
+                msg = f"Type {pre}help to see available commands, or visit my Twitch page for details."
+            else:
+                msg = f'{pre}apply username <-- Type this, using your own chess username, to apply!'
+            return await ctx.send(f"@{name}: {msg}")
+        elif isinstance(error, errors.MissingRequiredArgument):
+            # using apply badly
+            if error.param.name == 'chess_name':
+                msg = f'{pre}apply username <-- Type this, using your own chess username, to apply!'
+            # using set badly
+            elif error.param.name == 'setting':
+                sheet = self.sheets[ctx.channel.name]
+                msg = f"Current settings: {sheet.current_settings}"
+            elif error.param.name == 'value':
+                msg = BattleSheet.settings_help_string
+            else:
+                msg = str(error)
+            return await ctx.send(f"@{name}: {msg}")
+        return await super().event_command_error(ctx, error)
+
+    @check(is_bot_channel)
+    @command(name='join', no_global_checks=True)
+    async def join(self, ctx, channel_name=None):
+        """join - Make the bot join the user's channel"""
+        # Giving myself the option to make it join others' channels
+        if ctx.author.id != SED_ID:
+            channel_name = ctx.author.name.lower()
+        await self.join_channel(channel_name)
+        if channel_name in self.sheets:
+            await ctx.send(f"@{ctx.author.display_name}: I should be there now!")
+        else:
+            await ctx.send(f"@{ctx.author.display_name}: Hm, wait a bit and/or try again - something seems wrong")
+
+    @command(name='leave')
+    async def leave(self, ctx, channel_name=None):
+        """leave - Make the bot leave the channel (mods)"""
+        if ctx.author.id == SED_ID:
+            await self.leave_channel(channel_name)
+        else:
+            await self.leave_channel(ctx.channel.name)
+
+    @command(name='clear')
+    async def clear(self, ctx):
+        """clear - Reset the spreadsheet (mods)"""
+        sheet = self.sheets[ctx.channel.name]
+        await sheet.clear()
+
+    @command(name='link')
+    async def link(self, ctx):
+        """link - Post link to the spreadsheet"""
+        sheet = self.sheets[ctx.channel.name]
+        await ctx.send(f"Find the sheet at {sheet.url}")
+
+    @command(name='help')
+    async def help(self, ctx):
+        """help - Provide some assistance"""
+        await ctx.send(self.help_msg_template.substitute(prefix=ctx.prefix))
+
+    @command(name='set')
+    async def set(self, ctx, setting: str, value: str):
+        """set setting value - Change settings (mods). Use without arguments for current settings"""
+        sheet = self.sheets[ctx.channel.name]
+        try:
+            set_method = getattr(sheet, f"set_{setting}")
+            await set_method(value)
+
+        # not a valid setting
+        except AttributeError:
+            await ctx.send(BattleSheet.settings_help_string)
+
+        # not a valid value
+        except ValueError as e:
+            await ctx.send(f"@{ctx.author.display_name}: {e}")
+
+    @command(name='test')
+    async def test(self, ctx, arg='thing'):
+        print(ctx.author.display_name, ctx.author.id)
+
+    @command(name='apply', no_global_checks=True)
+    async def apply(self, ctx, chess_name):
+        """apply chess_name - Add user and chess stats to spreadsheet"""
+        user = ctx.author
+        twitch_name = user.display_name
+        sub = user.is_subscriber or 'founder' in user.badges
+        sheet = self.sheets[ctx.channel.name]
+        site = sheet.site
+        api = self.apis[site]
+        try:
+            # regrabbing chess_name to (possibly) collect correct casing from lookup
+            chess_name, rating, *peak_data = await api.lookup(chess_name, sheet.game)
+        except UserNotFound:
+            await ctx.send(f"@{twitch_name}: Couldn't find player \"{chess_name}\" on {site}!")
+        except APIError as e:
+            await ctx.send(f"@{twitch_name}: {e}")
+        except Exception as e:
+            # TODO: actual logging here
+            print(f"Unexpected error: {e}")
+            await ctx.send(f"Unexpected error, please let Sedsarq know!")
+        else:
+            await sheet.add_data(twitch_name, chess_name, rating, *peak_data, sub=sub)
+
+
+if __name__ == "__main__":
+
+    bot = SubBatBot(
+        irc_token=os.environ['TMI_TOKEN'],
+        client_id=os.environ['CLIENT_ID'],
+        nick=os.environ['BOT_NICK'],
+        prefix=os.environ['BOT_PREFIX'],
+        initial_channels=[os.environ['CHANNEL']],
+    )
+    bot.run()
---
+++ sheet.py
@@ -0,0 +1,172 @@
+import gspread_asyncio
+from oauth2client.service_account import ServiceAccountCredentials
+from gspread.exceptions import SpreadsheetNotFound
+from re import search
+
+
+def get_creds():
+    return ServiceAccountCredentials.from_json_keyfile_name("sheet_creds.json")
+
+
+agcm = gspread_asyncio.AsyncioGspreadClientManager(get_creds)
+
+
+class BattleSheet:
+    settings_help_string = "?set site lichess (or chess.com); " \
+                           "?set format bracket (or space, or none); " \
+                           "?set game bullet (or rapid, or blitz)"
+    def __init__(self, channel_name):
+        # Better to create through the async open method, which includes the actual sheet object
+        self.channel_name = channel_name
+        self.sheet = None
+        self.last_col = None
+        self._header_data = None
+        self.users_on_sheet = {}
+        self.url = None
+
+        # settings user can modify
+        self.format = None
+        self.site = None
+        self.game = None
+
+    @property
+    def current_settings(self):
+        return f"format={self.format}, site={self.site}, game={self.game}"
+
+    @classmethod
+    async def open(cls, channel_name):
+        battle_sheet = cls(channel_name)
+        await battle_sheet._connect_settings()
+        await battle_sheet._connect_sheet()
+        await battle_sheet.refresh_users()
+        return battle_sheet
+
+    async def _connect_settings(self):
+        #TODO: collect from DB here, defaults below
+        await self.set_format('none')
+        await self.set_site('chess.com')
+        self.game = 'blitz'
+
+    async def _connect_sheet(self):
+        agc = await agcm.authorize()
+        try:
+            self.sheet = await agc.open(self.channel_name)
+        except SpreadsheetNotFound:
+            self.sheet = await self.new_sheet(self.channel_name)
+        self.url = self.sheet.ss.url
+
+    async def new_sheet(self, sheet_name):
+        agc = await agcm.authorize()
+        sheet = await agc.create(sheet_name)
+        sheet.ss.share(None, perm_type='anyone', role='reader', notify=False, with_link=True)
+        ws1 = await sheet.get_worksheet(0)
+        ws1.ws.update_title('Subs')
+        ws2 = await sheet.add_worksheet('Not subs', 100, 28)
+        await self.make_header(ws1)
+        await self.make_header(ws2)
+        return sheet
+
+    async def make_header(self, worksheet):
+        await worksheet.batch_update([self._header_data])
+        # TODO: can this formatting line be awaited?
+        worksheet.ws.format(self._header_data['range'], {"textFormat": {"bold": True}})
+
+    async def set_format(self, value):
+        if value == self.format:
+            return
+        formats = ('none', 'space', 'bracket')
+        if value not in formats:
+            raise ValueError("Available formats: " + ', '.join(formats))
+        self.format = value
+        # TODO: update column here
+
+    async def set_site(self, value):
+        if value == self.site:
+            return
+        if value == 'chess.com':
+            header = ['Twitch', 'Chess.com', 'Current rating', 'Formatted', 'Peak rating', 'Peak date']
+        elif value == 'lichess':
+            header = ['Twitch', 'Lichess', 'Current rating', 'Formatted']
+        else:
+            raise ValueError(f"{value} is not an available site. Try lichess or chess.com")
+        self.site = value
+        self.last_col = chr(64 + len(header))
+        self._header_data = {
+            'range': f"A1:{self.last_col}1",
+            'values': [header]
+        }
+
+    async def set_game(self, value):
+        if value == self.game:
+            return
+        game_types = ('blitz', 'bullet', 'rapid')
+        if value not in game_types:
+            raise ValueError(f"Available game types are {', '.join(game_types)}")
+        self.game = value
+
+    async def add_data(self, twitch_name, chess_name, rating, *peak_values, sub=True):
+        if self.format == 'none':
+            format_name = '-'
+        elif self.format == 'bracket':
+            format_name = f"{chess_name} ({rating})"
+        elif self.format == 'space':
+            format_name = f"{chess_name} {rating}"
+        row_values = [twitch_name, chess_name, rating, format_name, *peak_values]
+        agc = await agcm.authorize()
+        sheet = await agc.open(self.channel_name)
+        if sub:
+            ws = await sheet.get_worksheet(0)
+        else:
+            ws = await sheet.get_worksheet(1)
+        last_entry = self.users_on_sheet.get(twitch_name)
+
+        # replace user data
+        if last_entry:
+            prev_ws, prev_row_nr = last_entry
+            if ws == prev_ws:
+                await self._replace(ws, prev_row_nr, row_values)
+
+            # user changed sub status
+            else:
+                await prev_ws.delete_row(prev_row_nr)
+                await self._append(ws, twitch_name, row_values)
+        # append new row
+        else:
+            await self._append(ws, twitch_name, row_values)
+
+    async def _append(self, ws, user_id, row_values):
+        ret = await ws.append_row(row_values)
+        row_nr = int(search(r'\d+$', ret['updates']['updatedRange']).group())
+        self.users_on_sheet[user_id] = ws, row_nr
+
+    async def _replace(self, ws, row_nr, values):
+        cells = await ws.range(f'A{row_nr}:{self.last_col}{row_nr}')
+        for cell, value in zip(cells, values):
+            cell.value = value
+        await ws.update_cells(cells)
+
+    async def remove(self):
+        agc = await agcm.authorize()
+        await agc.del_spreadsheet(self.sheet.id)
+
+    async def refresh_users(self):
+        d = {}
+        worksheets = await self.sheet.worksheets()
+        for ws in worksheets:
+            twitch_name_col = await ws.col_values(1)
+            twitch_name_to_row_nr = {name: (ws, n) for n, name in enumerate(twitch_name_col[1:], 2)}
+            d.update(twitch_name_to_row_nr)
+        self.users_on_sheet = d
+
+    async def clear(self):
+        worksheets = await self.sheet.worksheets()
+        for ws in worksheets:
+            await ws.clear()
+            await self.make_header(ws)
+        self.users_on_sheet = {}
+
+
+async def all_sheet_names():
+    agc = await agcm.authorize()
+    sheets = await agc.openall()
+    return [await sheet.get_title() for sheet in sheets]
