4b170b55ae393f8c76ca24d3c351488de2dc78bd
Sedsarq <s3dsarq@gmail.com>
2020-09-01 10:37:17 +0200

    add acc ids to globals

--- globals.py
+++ globals.py
@@ -2,6 +2,8 @@ from os import environ
 
 DEV_MODE = environ['BOT_NICK'].lower() == 'sbbdev'
 SED_ID = 88128608
+SBB_ID = 565663874
+SBBD_ID = 572514237
 
 # twitch usernames which are skipped if trying to use a command. bots should be skipped so
 # they can't be used to sneakily access mod commands
